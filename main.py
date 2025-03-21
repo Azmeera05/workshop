e9c74c5690f710331aa365058f4c02dfe1e9b242
Azmeera05 <manjuazmeera5@gmail.com>
2025-03-21 12:22:38 +0530

    changed

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Hello World')
+st.write('This is a simple program')
+
+st.button('say hello');
+st.text('hello, streamlit')
+st.text_input('Please Enter Your Name')
